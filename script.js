0244ee530c9cf06871613b4a8c0f960c6461d49d
Miquel Mauri Richarte <mmauridev@gmail.com>
2025-05-05 19:53:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function showMessage() {
     let resultado = "Hoy es el cumpleaños de la chica más lista que conozco, feliz cumpleaños belenita";
     document.getElementById("message").innerHTML = resultado;
 }
+
+function selectFirstNumber(num) {
+    document.getElementById("firstNumberDD").innerHTML = `<b>${num}</b>`;
+}
+
+function selectSecondNumber(num) {
+    document.getElementById("secondNumberDD").innerHTML = `<b>${num}</b>`;
+}
+
