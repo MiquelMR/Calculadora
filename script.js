de1d7896b406bf2e706145c58e83592f210da6fd
Miquel Mauri Richarte <mmauridev@gmail.com>
2025-05-05 19:43:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    let resultado = "Hoy es el cumpleaños de la chica más lista que conozco, feliz cumpleaños belenita";
+    document.getElementById("message").innerHTML = resultado;
+}
